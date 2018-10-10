56422e28a09215b31d1d24292e257e59282d3d27
sreekanth kotra <sreekanth.kotra@hotmail.com>
2018-10-10 22:26:12 +0530

    10 oct changes

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
+//Load user model
+require('../models/user');
+const User = mongoose.model('users')
+
+
 //user login
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
   res.render('users/login');
 })
 
 //user Register
-router.get('/register',(req,res)=>{
+router.get('/register', (req, res) => {
   res.render('users/register');
+})
+
+//Register form post
+router.post('/register', (req, res) => {
+  let errors = [];
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Passwords do not match' });
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: 'Passwords must be at least 4 characters' });
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.fristname,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    })
+
+  } else {
+    let newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password:req.body.password,
+      password2:req.body.password2
+    }
+    new User(newUser).save()
+      .then(idea => {
+        req.flash('success_msg', 'New user  added');
+        res.redirect('/users/login');
+      })
+
+  }
+
 })
 
 module.exports = router;
